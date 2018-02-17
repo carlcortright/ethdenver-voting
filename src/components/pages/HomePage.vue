8e4f1cec476b57e1fcdce9ea9b51df6693b3263b
Gerard Casas Saez <casassg@gmail.com>
2018-02-17 11:36:12 -0700

    add rly simple interface for candidates

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -2,30 +2,31 @@
   <div>
     <h1>TODO</h1>
     <p>Make it work.</p>
+    <ol>
+      <li v-for="cand in candidates">
+        {{ cand.description }}
+      </li>
+    </ol>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'home-page',
-  data () {
-    this.test()
-    return {}
-  },
-  components: {
-  },
-  methods: {
-    test () {
-      this.$vote.getCandidates().then((candidates) => {
-        console.log(candidates)
-      }).catch(error => {
-        console.error(error)
+  export default {
+    name: 'home-page',
+    data () {
+      return {
+        'candidates': []
+      }
+    },
+    mounted () {
+      this.$vote.getCandidates().then((cands) => {
+        this.candidates = cands
       })
-    }
+    },
+    components: {},
+    methods: {}
   }
-}
 </script>
 
 <style lang="scss" scoped>
-// some styles
 </style>
