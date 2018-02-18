2edc1aa8cae9ceaed2c427c54d7da87a43704388
sprusr <scott@spru.sr>
2018-02-18 06:47:25 -0700

    Deploy

---
+++ sw.js
@@ -0,0 +1,4 @@
+var serviceWorkerOption = {"assets":["/static/js/vendor.d2e866cd5fb2c675cc48.js","/static/js/app.32c2e4d8e142ef311d21.js","/static/js/manifest.8e014f2c4786741c02fe.js","/static/css/app.e4a4ca12aeffe380bf6e02dba7c176e7.css"]};
+        
+        !function(n){function r(e){if(t[e])return t[e].exports;var o=t[e]={i:e,l:!1,exports:{}};return n[e].call(o.exports,o,o.exports,r),o.l=!0,o.exports}var t={};r.m=n,r.c=t,r.i=function(n){return n},r.d=function(n,t,e){r.o(n,t)||Object.defineProperty(n,t,{configurable:!1,enumerable:!0,get:e})},r.n=function(n){var t=n&&n.__esModule?function(){return n.default}:function(){return n};return r.d(t,"a",t),t},r.o=function(n,r){return Object.prototype.hasOwnProperty.call(n,r)},r.p="",r(r.s="3gGW")}({"3gGW":function(n,r){}});
+//# sourceMappingURL=sw.js.map
